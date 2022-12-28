6b3e5ac1e71223d891c440060fc78d66f3873850
SANKET <32214164+sanket8088@users.noreply.github.com>
2022-12-28 22:14:18 +0530

    added pagination

--- products/views/fetch_all_category_view.py
+++ products/views/fetch_all_category_view.py
@@ -5,6 +5,7 @@ from user.serializer import AddressRequest
 from products.models import Categories
 from rest_framework.permissions import IsAuthenticated
 from django.db import transaction
+from django.core.paginator import Paginator
 
 
 # create a functionality to set default address
@@ -18,11 +19,35 @@ class FetchAllCategoriesView(APIView):
     def get(self, request):
         user = request.user
         category_qs = Categories.objects.filter(is_deleted = False)
-        resp = []
-        for category in category_qs:
-            resp.append({"id" : category.id, "name" : category.name, "description" : category.description})
-        return Response({"data" : resp}, status = 200)
+        page = request.GET.get("p", 1)
+        psz = request.GET.get("psz", 10)
+        paginator_object = Paginator(category_qs, psz) #get total context of data, count, total_page_number
+        object_list = paginator_object.page(page) #actual data of paginated form
+        # import pdb; pdb.set_trace()
+        page_info = {"count" : paginator_object.count, "total_pages" : paginator_object.num_pages, "cur_page" : page}
+        if object_list.has_next():
+            page_info["next"] = object_list.next_page_number()
+        else:
+            page_info["next"] = None
+        if object_list.has_previous():
+            page_info["previous"] = object_list.previous_page_number()
+        else:
+            page_info["previous"] = None
 
+        resp = []
+        for data in object_list:
+            resp.append({"id" : data.id, "name" : data.name, "description" : data.description})
+            
+        return Response({"page_info" : page_info, "data" : resp}, status = 200)
+
+
+
+# 1. url parameter - Mandatory
+# 2. Query params - Non mandatory
+
+# 21  1 1-10
+#     2 11-20
+#     3 21-30
 
 
 
